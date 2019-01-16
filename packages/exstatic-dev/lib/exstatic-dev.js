7d4b07b4cdfd9be3b8432805803e3e46d07aad37
Vikas <vikaspotluri123.github@gmail.com>
2019-01-16 10:55:06 -0600

    add basic watch command

--- packages/exstatic-dev/lib/exstatic-dev.js
+++ packages/exstatic-dev/lib/exstatic-dev.js
@@ -1,7 +1,30 @@
-const Exstatic = require('exstatic');
+const {Exstatic} = require('exstatic');
+const {watch} = require('./extends');
 
 class ExstaticDev extends Exstatic {
+	constructor(...args) {
+		super(...args);
+		this.exitActions = [];
+	}
 
+	watch() {
+		return watch.call(this);
+}
+
+	registerExitHooks(...args) {
+		Exstatic.prototype.registerExitHooks.apply(this, args);
+
+		this.realOnBeforeExit = ((...args) => {
+			this.exitActions.forEach(action => action());
+			this.onBeforeExit(...args);
+		}).bind(this);
+
+		process.off('SIGINT', this.onBeforeExit)
+			.off('SIGTERM', this.onBeforeExit)
+			.on('SIGINT', this.realOnBeforeExit)
+			.on('SIGTERM', this.realOnBeforeExit);
+	}
 }
 
 module.exports = opts => new ExstaticDev(opts);
+module.exports.ExstaticDev = ExstaticDev;
---
+++ packages/exstatic-dev/lib/extends/watch.js
@@ -0,0 +1,85 @@
+const path = require('path');
+const chokidar = require('chokidar');
+const {unlink, stat} = require('fs');
+const {Exstatic} = require('exstatic');
+const File = require('exstatic/lib/file');
+const log = require('exstatic/lib/log');
+
+module.exports = function watchForChanges() {
+	if (!(this instanceof Exstatic)) {
+		throw new TypeError('Incorrect usage of watch - did not received Exstatic instance');
+	}
+
+	const foldersToWatch = [this.files.inputDir];
+
+	if (!this.files.layoutsDir.includes(this.files.inputDir)) {
+		foldersToWatch.push(this.files.layoutsDir);
+	}
+
+	if (!this.files.partialsDir.includes(this.files.inputDir)) {
+		foldersToWatch.push(this.files.partialsDir);
+	}
+
+	log.verbose(`Watching folders: ${foldersToWatch.join(', ')}`);
+
+	const watcher = chokidar.watch(foldersToWatch, {
+		persistent: true
+	});
+
+	watcher.on('ready', () => {
+		watcher.on('add', absolutePath => {
+			log.verbose(`File added`, absolutePath);
+			const file = new File({
+				location: absolutePath,
+				directory: this.files.inputDir,
+				url: this.data.site.url,
+				tempFolder: this.files.tempDir,
+				compiler: this._hbs.generateCompiler.bind(this._hbs)
+			})
+			file.write(this.files.outputDir);
+			this.docs.core.push(file);
+		});
+
+		watcher.on('change', absolutePath => {
+			log.verbose(`File changed`, absolutePath);
+			let check = false;
+			if (absolutePath.includes(this.files.layoutsDir) || absolutePath.includes(this.files.partialsDir)) {
+				log.info('Layout or partial change detected, rebuilding everything');
+				check = true;
+			}
+
+			this.docs.core.forEach(file => {
+				if (check || file.path === absolutePath) {
+					log.info(`Rebuilding File ${file.path}`);
+					file.reload('write');
+				}
+			})
+		});
+
+		watcher.on('remove', absolutePath => {
+			log.verbose(`File removed`, absolutePath);
+			log.info('Removing File', absolutePath);
+			let index = -1;
+			this.docs.core.forEach((file, idx) => {
+				if (file.path === absolutePath) {
+					index = idx;
+				}
+			});
+
+			if (index > 0) {
+				const {wroteTo} = this.docs.core[index];
+				if (wroteTo) {
+					unlink(wroteTo);
+					// @todo: remove directory if empty
+				}
+
+				this.docs.core.splice(index, 0);
+			}
+		});
+	});
+
+	this.exitActions = [() => {
+		log.verbose('Disabling folder watch');
+		watcher.close();
+	}]
+}
