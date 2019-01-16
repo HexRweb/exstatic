4e5ebc8a15cce876eea27dd9193f285b11e9ba48
Vikas <vikaspotluri123.github@gmail.com>
2019-01-16 15:09:10 -0600

    Add xo + lint

--- packages/exstatic-cli/commands/watch.js
+++ packages/exstatic-cli/commands/watch.js
@@ -1,6 +1,5 @@
 const exstatic = require('exstatic-dev');
 const Error = require('../lib/exstatic-error');
-const {readFile} = require('fs').promises;
 
 module.exports = {
 	desc: 'Rebuild affected files when you make a change',
@@ -10,13 +9,13 @@ module.exports = {
 			describe: 'directory to run in'
 		}
 	},
-	handler: async function (argv) {
-		if (argv.d) {
+	async handler({d}) {
+		if (d) {
 			try {
-				process.chdir(argv.d);
+				process.chdir(d);
 			} catch (error) {
 				const message = error.code === 'ENOENT' ? 'it does not exist' : error.message;
-				throw new Error(`Unable to run in ${argv.d} - ${message}`);
+				throw new Error(`Unable to run in ${d} - ${message}`);
 			}
 		}
 
@@ -31,6 +30,7 @@ module.exports = {
 		} catch (error) {
 			throw new Error(`Watch failed - ${error.message}`);
 		}
+
 		return instance.watch();
 	}
-}
+};
