fa1badfb9e2862346a24af1583d4184e67358911
Vikas <vikaspotluri123.github@gmail.com>
2019-01-28 13:16:05 -0600

    remove quoting in utils exporter

--- packages/core/lib/utils/index.js
+++ packages/core/lib/utils/index.js
@@ -1,9 +1,9 @@
 module.exports = {
-	'ensureArray': require('./ensure-array'),
-	'file': require('./file'),
-	'getAllFiles': require('./get-all-files'),
-	'mergeHelpers': require('./merge-helpers'),
-	'normalize': require('./normalize'),
-	'readConfig': require('./read-config'),
-	'yamlParser': require('./yaml-parser')
+	ensureArray: require('./ensure-array'),
+	file: require('./file'),
+	getAllFiles: require('./get-all-files'),
+	mergeHelpers: require('./merge-helpers'),
+	normalize: require('./normalize'),
+	readConfig: require('./read-config'),
+	yamlParser: require('./yaml-parser')
 };
