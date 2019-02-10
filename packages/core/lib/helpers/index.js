936b15f40fc6b841eb5a9903a0b2d703e68d4bb6
Vikas <vikaspotluri123.github@gmail.com>
2019-02-10 12:59:27 -0600

    Add navigation helper

--- packages/core/lib/helpers/index.js
+++ packages/core/lib/helpers/index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	sync: {
 		all: require('./all'),
+		navigation: require('./navigation'),
 		classes: require('./classes'),
 		url: require('./url'),
 		excerpt: require('./excerpt'),
---
+++ packages/core/lib/helpers/navigation.js
@@ -0,0 +1,21 @@
+const slugify = require('slugify');
+
+module.exports = function navigation({fn}) {
+	const navigation = this.instance.hbs.data('site.navigation');
+	if (!Array.isArray(navigation)) {
+		throw new Error('Navigation does not exist');
+	}
+
+	let output = '';
+
+	navigation.forEach(({title, href}) => {
+		// @todo: determine how sub-pages should be handled
+		// For example: If /blog/hello-world is being compiled, should it be possible to allow
+		// `/blog` to be considered active? If so, what's the best way to implement it?
+		const current = href.replace(/^\/|\/$/, '') === this.page.path.replace(/^\/|\/$/, '');
+		const slug = slugify(title.toLowerCase());
+		output += fn({title, href, current, slug});
+	});
+
+	return new this.instance.hbs.SafeString(output);
+};
