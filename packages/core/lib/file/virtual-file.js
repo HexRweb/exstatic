90db7ec1af35fcca6071a2bdec1859ad2ff3b7fe
Vikas <vikaspotluri123.github@gmail.com>
2019-02-12 18:20:37 -0600

    Finish initial virtual file implementation

--- packages/core/lib/file/virtual-file.js
+++ packages/core/lib/file/virtual-file.js
@@ -1,11 +1,19 @@
 const assert = require('assert');
+const path = require('path');
+const {normalize, file} = require('../utils');
 const AbstractFile = require('./abstract-file');
+
+const {fileName} = file;
 
 module.exports = class VirtualFile extends AbstractFile {
 	constructor(options = {}) {
-		super();
+		super(options);
 
 		assert.ok(options.meta);
+		assert.ok(options.data);
+
 		this.meta = options.meta;
+		this.rendered = options.data;
+		this.filename = normalize(path.resolve(this.output, fileName(this.source, Boolean(this.meta.path))));
 	}
 };
