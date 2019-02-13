13d9c62aa12d40da41c90af9bd271da9b9783d3a
Vikas <vikaspotluri123.github@gmail.com>
2019-02-12 18:54:13 -0600

    :bug: core: load-pages resolver - use hook result

--- packages/core/lib/hooks/load-pages.js
+++ packages/core/lib/hooks/load-pages.js
@@ -2,7 +2,7 @@ const {ensureArray} = require('../utils');
 
 // @note: this function mutates the provided array
 module.exports = function mergeLocalAndVirtualPages(hookResults, docs) {
-	hookResults.forEach(newPages =>
-		ensureArray(newPages).forEach(newPage => docs.push(newPage))
+	hookResults.forEach(({result}) =>
+		ensureArray(result).forEach(newPage => docs.push(newPage))
 	);
 };
