497167e3efef03412ac9dec5f18f6cf77965f631
Vikas <vikaspotluri123.github@gmail.com>
2019-03-02 15:28:27 -0600

    utils: fix lint

--- packages/utils/lib/normalize-path.js
+++ packages/utils/lib/normalize-path.js
@@ -1,5 +1,6 @@
 const {normalize} = require('path');
 
 module.exports = function normalizeFilePaths(fileName) {
+	// eslint-disable-next-line no-useless-escape
 	return normalize(fileName.replace(/\.\.[\/\\]?/g, '')).replace(/\\/g, '/');
 };
