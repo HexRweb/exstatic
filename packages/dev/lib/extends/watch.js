8f6577e5c34bdd2d0c06f7730aaa666844cb0afb
Vikas <vikaspotluri123.github@gmail.com>
2019-03-04 15:33:10 -0600

    dev watch: move handlers outside of core

--- packages/dev/lib/extends/watch.js
+++ packages/dev/lib/extends/watch.js
@@ -3,24 +3,96 @@ const {Exstatic} = require('@exstatic/core');
 const {log} = require('@exstatic/logging');
 const {normalizePath: normalize, fs} = require('@exstatic/utils');
 
-function noSlashes(input) {
-	return input
-		.replace(/^\/+/, '')
-		.replace(/\/+$/, '');
+const replaceAndNormalize = (input, bad) => input.replace(bad, '').replace(/^\/+/, '').replace(/\/+$/, '');
+
+const isConfig = (instance, fullPath) => fullPath.includes(instance.fm.config);
+const isLayout = (instance, fullPath) => fullPath.includes(instance.fm.layoutsDir);
+const isPartial = (instance, fullPath) => fullPath.includes(instance.fm.partialsDir);
+
+const removePageRoot = (instance, fullPath) => replaceAndNormalize(fullPath, instance.fm.inputDir);
+const removeLayoutRoot = (instance, fullPath) => replaceAndNormalize(fullPath, instance.fm.layoutsDir);
+const removePartialRoot = (instance, fullPath) => replaceAndNormalize(fullPath, instance.fm.partialsDir);
+
+
+async function handleAdd(absolutePath) {
+	absolutePath = normalize(absolutePath);
+
+	if (isLayout(absolutePath)) {
+		return log.info(`Layout added: ${removeLayoutRoot(this, absolutePath)}`);
+	}
+
+	if (isPartial(absolutePath)) {
+		return log.info(`Partial added: ${removePartialRoot(this, absolutePath)}`);
+	}
+
+	log.info(`Page added: ${removePageRoot(this, absolutePath)}`);
+
+	const file = this.fm.addFile(absolutePath);
+	this.refreshFile(file);
+}
+
+function handleChange(absolutePath) {
+	absolutePath = normalize(absolutePath);
+
+	if (isConfig(this, absolutePath)) {
+		log.info('Config file changed, reloading');
+		return this.events.emit('TRIGGER_RESTART');
+	}
+
+	if (isLayout(this, absolutePath)) {
+		log.info(`Layout ${removeLayoutRoot(this, absolutePath)} changed; rebuilding everything`);
+		return this.refreshAll();
+	}
+
+	if (isPartial(this, absolutePath)) {
+		log.info(`Partial ${removePartialRoot(this, absolutePath)} changed; rebuilding everything`);
+		return this.refreshAll();
+	}
+
+	log.info(`Refreshing page ${removePartialRoot(this, absolutePath)}`);
+	return this.refreshFile(absolutePath);
+}
+
+async function handleUnlink(absolutePath) {
+	absolutePath = normalize(absolutePath);
+	const layoutChanged = isLayout(this, absolutePath);
+	const partialChanged = isPartial(this, absolutePath);
+
+	if (layoutChanged || partialChanged) {
+		const meta = layoutChanged ? `Layout ${removeLayoutRoot(this, absolutePath)}` :
+			`Partial ${removePartialRoot(this, absolutePath)}`;
+		log.info(`${meta} removed; rebuilding everything`);
+
+		return Promise.all(this.fm.files.map(file => {
+			log.info(`Rebuilding file ${removePageRoot(this, file.source)}`);
+			return file.reload();
+		}));
+	}
+
+	const index = this.fm.files.findIndex(file => file.source === absolutePath);
+
+	if (index < 0) {
+		return;
+	}
+
+	log.info(`Removed ${removePageRoot(this, absolutePath)}`);
+	const file = this.fm.files[index];
+	this.fm.files.splice(index, 1);
+
+	if (file.filename) {
+		await fs.unlink(file.filename).catch(() => true);
+		// @todo: remove directory if empty
+	}
+
+	if (file.tempContext) {
+		await file.temp.release(file.tempContext);
+	}
 }
 
 module.exports = function watchForChanges() {
 	if (!(this instanceof Exstatic)) {
 		throw new TypeError('Incorrect usage of watch - did not received Exstatic instance');
 	}
-
-	const isLayout = fullPath => fullPath.includes(this.fm.layoutsDir);
-	const isPartial = fullPath => fullPath.includes(this.fm.partialsDir);
-	const isConfig = fullPath => fullPath.includes(this.fm.config);
-
-	const removeLayoutRoot = fullPath => noSlashes(fullPath.replace(this.fm.layoutsDir, ''));
-	const removePartialRoot = fullPath => noSlashes(fullPath.replace(this.fm.partialsDir, ''));
-	const removePageRoot = fullPath => noSlashes(fullPath.replace(this.fm.inputDir, ''));
 
 	const foldersToWatch = [this.fm.inputDir];
 
@@ -39,96 +111,14 @@ module.exports = function watchForChanges() {
 	const watcher = chokidar.watch(foldersToWatch, {persistent: true});
 
 	watcher.on('ready', () => {
-		watcher.on('add', async absolutePath => {
-			absolutePath = normalize(absolutePath);
-
-			if (isLayout(absolutePath)) {
-				return log.info(`Layout added: ${removeLayoutRoot(absolutePath)}`);
-			}
-
-			if (isPartial(absolutePath)) {
-				return log.info(`Partial added: ${removePartialRoot(absolutePath)}`);
-			}
-
-			log.info(`Page added: ${removePageRoot(absolutePath)}`);
-
-			const file = this.fm.addFile(absolutePath);
-			this.refreshFile(file);
-		});
-
-		watcher.on('change', absolutePath => {
-			absolutePath = normalize(absolutePath);
-			let globalChange = false;
-
-			if (isConfig(absolutePath)) {
-				log.info('Config file changed, reloading');
-				return this.events.emit('TRIGGER_RESTART');
-			}
-
-			if (isLayout(absolutePath)) {
-				log.info(`Layout ${removeLayoutRoot(absolutePath)} changed; rebuilding everything`);
-				globalChange = true;
-			} else if (isPartial(absolutePath)) {
-				log.info(`Partial ${removePartialRoot(absolutePath)} changed; rebuilding everything`);
-				globalChange = true;
-			}
-
-			if (globalChange) {
-				return this.refreshAll();
-			}
-
-			return this.refreshFile(absolutePath);
-		});
-
-		watcher.on('unlink', async absolutePath => {
-			absolutePath = normalize(absolutePath);
-			let rebuild = false;
-
-			if (isLayout(absolutePath)) {
-				log.info(`Layout ${removeLayoutRoot(absolutePath)} removed; rebuilding everything`);
-				rebuild = true;
-			}
-
-			if (isPartial(absolutePath)) {
-				log.info(`Partial ${removePartialRoot(absolutePath)} removed; rebuilding everything`);
-				rebuild = true;
-			}
-
-			if (rebuild) {
-				return this.fm.files.forEach(file => {
-					log.info(`Rebuilding file ${removePageRoot(file.source)}`);
-					file.reload();
-				});
-			}
-
-			const index = this.fm.files.findIndex(file => file.source === absolutePath);
-
-			if (index < 0) {
-				return;
-			}
-
-			log.info(`Removed ${removePageRoot(absolutePath)}`);
-			const file = this.fm.files[index];
-			this.fm.files.splice(index, 1);
-
-			if (file.filename) {
-				await fs.unlink(file.filename).catch(() => true);
-				// @todo: remove directory if empty
-			}
-
-			if (file.tempContext) {
-				await file.temp.release(file.tempContext);
-			}
-		});
+		watcher.on('add', handleAdd.bind(this));
+		watcher.on('change', handleChange.bind(this));
+		watcher.on('unlink', handleUnlink.bind(this));
 
 		log.info(`Watching:\n\t- ${foldersToWatch.join('\n\t- ')}`);
 		log.info('Waiting for changes');
 	});
 
-	this.exitActions = [() => {
-		log.verbose('Disabling folder watch');
-		watcher.close();
-	}];
-
+	this.exitActions.push(() => watcher.close());
 	return this;
 };
