387dae28ab8536dcfddba064c428e3c0dc68cd1c
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-01-19 12:25:11 -0600

    util.error: add support for coercing generic errors into Exstatic Errors

--- packages/utils/lib/error.js
+++ packages/utils/lib/error.js
@@ -1,9 +1,25 @@
+const {inspect} = require('util');
+
 module.exports = class ExstaticError extends Error {
+	static coerce(error, code) {
+		const _error = new this(error.message, code);
+		_error.originalError = error;
+		return _error;
+	}
+
 	constructor(message, code) {
 		super(message);
 
 		if (code) {
 			this.code = code;
 		}
+	}
+
+	toString() {
+		if (this.originalError) {
+			return inspect(this.originalError);
+		}
+
+		return super.toString();
 	}
 };
